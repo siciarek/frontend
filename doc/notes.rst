bca9f1b3037c99f9f421c820d28fa9aa307c49cc
Jacek Siciarek <jacek.siciarek@escore.pl>
2016-04-08 17:09:54 +0200

    Added validators, includes and some doc

---
+++ doc/notes.rst
@@ -0,0 +1,15 @@
+AngularJS tutorials references
+------------------------------
+
+    * http://mrzepinski.pl/angularjs-tutorial
+    * http://www.w3schools.com/angular/
+
+
+
+Important issues
+
+    * Directves
+    * Service types (Factory, Service, Provider)
+    * Includes
+    * AngularJS API https://docs.angularjs.org/api/ng/function
+    * Validation
--- src/AppBundle/Resources/public/jasmine/js/application/application.js
+++ src/AppBundle/Resources/public/jasmine/js/application/application.js
@@ -1,18 +1,77 @@
-var APP = angular.module('APP',[]);
+var APP = angular.module('APP', []);
 
-APP.controller('DefaultController', ['$scope', function($scope) {
-    $scope.value = null;
+APP.controller('DefaultController', ['$scope', function ($scope) {
+        $scope.data = {
+            name: 'Zażółć gęślą jaźń',
+            items: [
+                {
+                    id: 1,
+                    name: 'Zażółć'
+                },
+                {
+                    id: 2,
+                    name: 'Gęślą'
+                },
+                {
+                    id: 3,
+                    name: 'Jaźń'
+                }
+            ]
+        };
 
-    $scope.setValue = function(value) {
-        $scope.value = value;
-    };
-
-    $scope.setValue = function(value) {
-        $scope.value = value;
-    };
-
-    $scope.resetValue = function() {
         $scope.value = null;
-    };
-}]);
 
+        $scope.setValue = function (value) {
+            $scope.value = value;
+        };
+
+        $scope.setValue = function (value) {
+            $scope.value = value;
+        };
+
+        $scope.resetValue = function () {
+            $scope.value = null;
+        };
+    }]);
+
+APP.directive('notificationInfo', function () {
+    return {
+        templateUrl: 'templates/notification/info.html',
+        restrict: 'E',
+        replace: true
+    };
+});
+
+APP.directive('notificationError', function () {
+    return {
+        templateUrl: 'templates/notification/error.html',
+        restrict: 'E',
+        replace: true
+    };
+});
+
+APP.directive('list', function () {
+    return {
+        templateUrl: 'templates/list.html',
+        restrict: 'E',
+        replace: true
+    };
+});
+
+APP.directive('typePesel', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, control) {
+            var validation = function (value) {
+
+                // Simple PESEL validation:
+                var valid = value.match(/^\d{11}$/) !== null;
+
+                control.$setValidity('pesel', valid);
+                return value;
+            };
+
+            control.$parsers.push(validation);
+        }
+    };
+});
